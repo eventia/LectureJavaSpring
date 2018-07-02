e5e685e21a54fd0f8fbaa9ce441e2f24eed39907
eventia <eventia@gmail.com>
2018-07-03 01:52:50 +0900

    Spring AY 180703-[01:52]

--- spring_23_2_ex1_springex/src/main/java/com/javalec/spring_pjt_ex/command/ITicketCommand.java
+++
@@ -1,9 +0,0 @@
-package com.javalec.spring_pjt_ex.command;
-
-import com.javalec.spring_pjt_ex.dto.TicketDto;
-
-public interface ITicketCommand {
-
-	public void execute(TicketDto ticketDto);
-	
-}
